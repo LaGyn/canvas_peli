8e8ec6d4129e04ec49da314eac5d09109e21b3b4
Laura Gynther <lauragynther@live.com>
2022-09-05 11:42:55 +0300

    liiku()

--- main.js
+++ main.js
@@ -2,23 +2,9 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 canvas.fillStyle = "orange";
-canvas.fill();
+ctx.fill();
 
-class Mato {
-    constructor({position, speed}){
-        this.position = position;
-        this.speed = speed;
-    }
-
-    draw(){
-    ctx.beginPath();
-    ctx.arc(15, 15, 15, 0, Math.PI * 2);
-    ctx.fillStyle = 'red';
-    ctx.fill();
-    ctx.closePath();
-    }
-}
-
+document.addEventListener('keydown', liiku)
 
 function checkSupported() {
     if (canvas.getContext){
@@ -28,22 +14,47 @@ function checkSupported() {
       // Canvas is not supported
       alert("We're sorry, but your browser does not support the canvas tag. Please use any web browser other than Internet Explorer.");
     }
-  }
+  } 
 
-function Position(position, speed){
-    
+class Mato {
+   constructor({sijainti, vauhti, suunta}){
+        this.sijainti = sijainti;
+        this.vauhti = vauhti;
+        this.suunta = suunta;
+    }
+    draw(){
+    ctx.beginPath();
+    ctx.arc(15, 15, 15, 0, Math.PI * 2);
+    ctx.fillStyle = 'red';
+    ctx.fill();
+    ctx.closePath();
+    }
 }
+
+const mato = new Mato({
+    sijainti: {
+        x: 15,
+        y: 15
+    },
+    suunta: {
+        x: 1,
+        y: 1
+    },
+    vauhti: 5,
+    
+})
 
 // Madon liikkuminen:
 
-function move(){
-    let positionX = 15;
-    let positionY = 15;
-    let speed = 5;
+function liiku(event){
+    if (event.keyCode == 39){ // Nuoli oikealle
+        sijainti = this.sijainti.x + this.suunta.x * this.vauhti;
+    }
 
 }
 
 function pelaa() {
     document.getElementById("pelinappi").style.display = "none";
     document.getElementById("aloitus").style.display = "none";
+    mato.draw();
 }
