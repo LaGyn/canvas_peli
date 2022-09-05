1afad437f3c32d6ab226b45c7013779b2a947a29
juhr01 <juho1.riipinen@gmail.com>
2022-09-05 12:54:31 +0300

    ruokaa spawnaa random mestoihin

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-canvas.fillStyle = "orange";
+this.gridSize = 30;
+madonBody = [];
 ctx.fill();
 
 document.addEventListener('keydown', liiku)
@@ -40,10 +41,28 @@ function liiku(event){
 function pelaa() {
     document.getElementById("pelinappi").style.display = "none";
     document.getElementById("aloitus").style.display = "none";
+    luoRuokaPallo();
     mato.draw();
 }
 
 function peliOhi() {
   document.getElementById("canvas").style.display = "none";
   document.getElementById("lopetus").style.display = "block";
+}
+
+function luoRuokaPallo() {
+  suggestedPoint = [Math.floor(Math.random()*(canvas.width/gridSize))*gridSize, Math.floor(Math.random()*(canvas.height/gridSize))*gridSize];
+  if (madonBody.some(onPiste)) {
+    luoRuokaPallo();
+  } else {
+    ctx.fillStyle = "rgb(20, 200, 10)";
+    ctx.beginPath();
+    ctx.arc(suggestedPoint[0], suggestedPoint[1], 15, 0, Math.PI * 2, true);
+    ctx.fill();
+    ctx.closePath();
+  }
+}
+
+function onPiste(element, index, array) {
+  return (element[0] == suggestedPoint[0] && element[1] == suggestedPoint[1]);
 }
