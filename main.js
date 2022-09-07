ccab13d3cf320cfb32fb7e38192a719129f360bb
juhr01 <juho1.riipinen@gmail.com>
2022-09-07 12:18:24 +0300

    lähestulkoon valmis

--- main.js
+++ main.js
@@ -21,11 +21,11 @@ const mato = [
 // Selaintuen tutkiminen: 
 
 function checkSupported() {
-    if (!canvas.getContext){
-      alert("Selaimesi ei tue canvas-tagia!");
-      return;
-    } 
-  } 
+  if (!canvas.getContext) {
+    alert("Selaimesi ei tue canvas-tagia!");
+    return;
+  }
+}
 
 // Jos suuntaa vaihdetaan, niin on true
 let suunnanVaihto = false;
@@ -41,20 +41,25 @@ document.addEventListener("keydown", vaihdaSuunta);
 
 function pelaa() {
   checkSupported();
-    document.getElementById('pelinappi').style.display = 'none';
-    document.getElementById('aloitus').style.display = 'none';
-    document.getElementById('pisteruutu').style.display = 'block';
-    document.getElementById('canvas').style.display = 'block';
-    main();
-    RuokaSijainti(); // Tulostetaan ruoka paikoilleen
+  document.getElementById('pelinappi').style.display = 'none';
+  document.getElementById('aloitus').style.display = 'none';
+  document.getElementById('pisteruutu').style.display = 'block';
+  document.getElementById('canvas').style.display = 'block';
+  main();
+  RuokaSijainti(); // Lasketaan ruualle sijainti
 }
 
 // Funktioiden kutsunta mainissa, ajastin:
 
 function main() {
-  if (peliOhi()) return;
-    suunnanVaihto = false;
-    setTimeout(function onTick() {
+  if (peliOhi()) {
+    document.getElementById("canvas").style.display = "none";
+    document.getElementById("pisteruutu").style.display = "none";
+    document.getElementById("lopetus").style.display = "block";
+    document.getElementById("pisteetLopussa").innerHTML = pisteet;
+  }
+  suunnanVaihto = false;
+  setTimeout(function onTick() {
     clearCanvas();
     liikuMato()
     piirraMato();
@@ -66,15 +71,15 @@ function main() {
 // Tyhjennetään canvas:
 
 function clearCanvas() {
-ctx.fillStyle = canvasBgColor;
-ctx.strokestyle = canvasBorder;
-ctx.fillRect(0, 0, canvas.width, canvas.height);
-ctx.strokeRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = canvasBgColor;
+  ctx.strokestyle = canvasBorder;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.strokeRect(0, 0, canvas.width, canvas.height);
 }
 
 // Piirretään matopallo:
 
-function piirraMatoOsa(matoOsa) {   
+function piirraMatoOsa(matoOsa) {
   ctx.beginPath();
   ctx.arc(matoOsa.x - 15, matoOsa.y - 15, 15, 0, Math.PI * 2, true);
   ctx.fillStyle = vari;
@@ -89,9 +94,9 @@ function piirraMato() {
 }
 
 // Madon liikkuminen://
-  
-function liikuMato(event){
-  const head = {x: mato[0].x + dx, y: mato[0].y + dy};
+
+function liikuMato(event) {
+  const head = { x: mato[0].x + dx, y: mato[0].y + dy };
   mato.unshift(head);
   if (mato[0].x === ruokaX && mato[0].y === ruokaY) {
     RuokaSijainti();
@@ -103,75 +108,69 @@ function liikuMato(event){
 
 // Suunnanvaihto:
 
-function vaihdaSuunta(event) {  
+function vaihdaSuunta(event) {
   const vasenNappi = 37;
   const oikeaNappi = 39;
   const ylosNappi = 38;
   const alasNappi = 40;
-  
+
   if (suunnanVaihto) return;
   suunnanVaihto = true;
   const nappiPainettu = event.keyCode;
   const ylos = dy === -30;
   const alas = dy === 30;
-  const oikealle = dx === 30;  
+  const oikealle = dx === 30;
   const vasemmalle = dx === -30;
 
-  if (nappiPainettu === vasenNappi && !oikealle)
-  {    
-      dx = -30;
-      dy = 0;  
+  if (nappiPainettu === vasenNappi && !oikealle) {
+    dx = -30;
+    dy = 0;
   }
 
-  if (nappiPainettu === ylosNappi && !alas)
-  {    
-      dx = 0;
-      dy = -30;
+  if (nappiPainettu === ylosNappi && !alas) {
+    dx = 0;
+    dy = -30;
   }
 
-  if (nappiPainettu === oikeaNappi && !vasemmalle)
-  {    
-      dx = 30;
-      dy = 0;
+  if (nappiPainettu === oikeaNappi && !vasemmalle) {
+    dx = 30;
+    dy = 0;
   }
 
-  if (nappiPainettu === alasNappi && !ylos)
-  {    
-      dx = 0;
-      dy = 30;
+  if (nappiPainettu === alasNappi && !ylos) {
+    dx = 0;
+    dy = 30;
   }
 }
 
 // Pelin loppuminen:
 
 function peliOhi() {
-  for (let i = 4; i < mato.length; i++){    
-    if (mato[i].x === mato[0].x && mato[i].y === mato[0].y) 
-    return true;
+  for (let i = 4; i < mato.length; i++) {
+    if (mato[i].x === mato[0].x && mato[i].y === mato[0].y)
+      return true;
   }
-  const vasenTormays = mato[0].x < 0;  
-  const oikeaTormays = mato[0].x > canvas.width - 10;
-  const ylaTormays = mato[0].y < 0;
-  const alaTormays = mato[0].y > canvas.height - 10;
- 
-  return vasenTormays ||  oikeaTormays || ylaTormays || alaTormays;
-  //document.getElementById("canvas").style.display = "none";
-  //document.getElementById("lopetus").style.display = "block";
+  const vasenTormays = mato[0].x < 30;
+  const oikeaTormays = mato[0].x > canvas.width;
+  const ylaTormays = mato[0].y < 30;
+  const alaTormays = mato[0].y > canvas.height;
+
+  return vasenTormays || oikeaTormays || ylaTormays || alaTormays;
 }
 
 // Ruualle arvotaan koordinaatit:
 
 function luoKoordinaatit(min, max) {
-  return Math.round((Math.random() * (max-min) + min) / 30) * 30;
+  return Math.round((Math.random() * (max - min) + min) / 30) * 30;
 }
 
 // Ruoka tulostetaan uudelle paikalle, mikäli mato on tavoittanut ruuan:
 
 function RuokaSijainti() {
-  ruokaX = luoKoordinaatit(15, canvas.width -15); // parametreina min ja max arvot, jotta ruoka ei tulostu osittain rajojen ulkopuolelle
-  ruokaY = luoKoordinaatit(15, canvas.height -15)
-  mato.forEach(function onkoSyonytRuuan(osa) {
-    if (osa.x == ruokaX && osa.y == ruokaY) { // Jos madon osa on ruokasijainnissa, on ruoka syöty, mato kasvaa ja tulostetaan uusi ruokasijainti
+  ruokaX = luoKoordinaatit(15, canvas.width - 15); // parametreina min ja max arvot, jotta ruoka ei tulostu osittain rajojen ulkopuolelle
+  ruokaY = luoKoordinaatit(15, canvas.height - 15)
+  mato.forEach(function onkoKohdalla(osa) {
+    if (osa.x == ruokaX && osa.y == ruokaY) { // Jos madon osa on ruokasijainnissa, lasketaan uusi ruokasijainti
       RuokaSijainti();
     }
   })
@@ -182,34 +181,14 @@ function RuokaSijainti() {
 function piirraRuoka() {
   ctx.fillStyle = "rgb(0, 168, 0)"
   ctx.beginPath();
-    ctx.arc(ruokaX - 15, ruokaY - 15, 15, 0, Math.PI * 2, true);
-    ctx.fill();
-    ctx.closePath();
+  ctx.arc(ruokaX - 15, ruokaY - 15, 15, 0, Math.PI * 2, true);
+  ctx.fill();
+  ctx.closePath();
 }
 
 // Lasketaan pisteet:
 
-function laskePisteet(){
+function laskePisteet() {
   pisteet += 10;
   document.getElementById('pisteetNyt').innerHTML = pisteet;
 }
-/*function luoRuokaPallo() {
-  suggestedPoint = [Math.floor(Math.random()*(canvas.width/gridSize))*gridSize, Math.floor(Math.random()*(canvas.height/gridSize))*gridSize];
-  if (mato.some(onPiste)) {
-    luoRuokaPallo();
-  } else {
-    ctx.fillStyle = "rgb(20, 200, 10)";
-    ctx.beginPath();
-    ctx.arc(suggestedPoint[0], suggestedPoint[1], 15, 0, Math.PI * 2, true);
-    ctx.fill();
-    ctx.closePath();
-  }
-}
-
-function onPiste(element, index, array) {
-  return (element[0] == suggestedPoint[0] && element[1] == suggestedPoint[1]);
-}
-
-function onSyonytItsensa() {
-  return(element[0] == currentPosition['x'] && element[1] == currentPosition['y']);
-} */
